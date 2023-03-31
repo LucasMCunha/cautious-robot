19de6fcd3e36f02d12b9413e8106036857f2e2b7
Lucas Cunha <lucascunha@10-41-49-95.0200.epoa.br.iosda.org>
2023-03-31 10:54:04 -0700

    .

---
+++ tste/tste/tsteApp.swift
@@ -0,0 +1,17 @@
+//
+//  tsteApp.swift
+//  tste
+//
+//  Created by Lucas Cunha on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct tsteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
